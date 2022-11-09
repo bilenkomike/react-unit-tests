b35c9f23278252029eb7a71c98e7c36fb44823a3
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-09 12:02:29 +0200

    greeting exsits actions tests

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Greeting = () => {
+    const [changedText,setChangedText] = useState(false);
+
+    const changeTextHandler = () => {
+        setChangedText(true);
+    }
+
+    return <div>
+        <h2>Hello World!</h2>
+        {!changedText && <p>Its good to see you</p>}
+        {changedText && <p>Changed!</p>}
+        <button onClick={changeTextHandler}></button>
+    </div>
+}
+
+
+export default Greeting;
